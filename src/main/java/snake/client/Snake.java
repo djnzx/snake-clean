5160d6f1207d2e9a0dcbdf85d41371fd65056e26
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-27 16:30:56 +0400

    -- Snake sort ideas

--- src/main/java/snake/client/Snake.java
+++ src/main/java/snake/client/Snake.java
@@ -2,7 +2,7 @@ package snake.client;
 
 import com.codenjoy.dojo.services.Point;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +18,46 @@ public class Snake {
     return new Snake(convert(unsorted));
   }
 
+  /**
+   * Elements:
+   *
+   * HEAD_DOWN('▼'),
+   * HEAD_LEFT('◄'),
+   * HEAD_RIGHT('►'),
+   * HEAD_UP('▲'),
+   * TAIL_END_DOWN('╙'),
+   * TAIL_END_LEFT('╘'),
+   * TAIL_END_UP('╓'),
+   * TAIL_END_RIGHT('╕'),
+   * TAIL_HORIZONTAL('═'),
+   * TAIL_VERTICAL('║'),
+   * TAIL_LEFT_DOWN('╗'),
+   * TAIL_LEFT_UP('╝'),
+   * TAIL_RIGHT_DOWN('╔'),
+   * TAIL_RIGHT_UP('╚'),
+   *
+   * GOOD_APPLE('☺'),
+   * BAD_APPLE('☻'),
+   *
+   * BREAK('☼'),
+   */
   public static Snake from(Board board) {
+    LinkedList<Point> body_unsorted = (LinkedList<Point>)board.getSnake();
+    LinkedList<LeePoint> body_sorted = new LinkedList<>();
+
+    Point head = body_unsorted.removeFirst();
+    body_sorted.add(new LeePoint(head));
+    Point prev = head;
+    while (body_unsorted.size() > 0) {
+      // find the next point
+      switch (board.getAt(prev)) {
+        case HEAD_UP:;
+        // ...
+      }
+      // ...
+    }
     // TODO: implement
-    return new Snake(new ArrayList<>());
+    return new Snake(body_sorted);
   }
 
   public Snake(List<LeePoint> data) {
